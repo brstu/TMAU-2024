9dad45918de7eecf6fde72519b8eb463a62beb42
teenage717 <ivan.kulikovich1035>
2024-10-26 11:15:36 +0300

    fix dublication

--- trunk/as0006312/task_01/scr/main.cpp
+++ trunk/as0006312/task_01/scr/main.cpp
@@ -9,58 +9,58 @@ struct SimulationData {
     vector<double> model2;
 };
 
-SimulationData executeSimulation(double a, double b, double c, double d, double initialTemp, const vector<double>& heatValues, int steps) {
+SimulationData runSimulation(double a, double b, double c, double d, double initialTemp, const vector<double>& inputs, int steps) {
     vector<double> result1(steps);
-    result1[0] = initialTemp; // Initial temperature
+    result1[0] = initialTemp;
 
     vector<double> result2(steps);
-    result2[0] = initialTemp; // Initial temperature
+    result2[0] = initialTemp;
 
     for (int t = 1; t < steps; ++t) {
-        result1[t] = a * result1[t - 1] + b * heatValues[t - 1]; // Update based on model 1
+        result1[t] = a * result1[t - 1] + b * inputs[t - 1]; // Model 1 update
 
         double prev_y = result2[t - 1];
-        double prev_u = heatValues[t - 1];
-        result2[t] = a * prev_y - b * pow(prev_y, 2) + c * prev_u + d * sin(prev_u); // Update based on model 2
+        double prev_u = inputs[t - 1];
+        result2[t] = a * prev_y - b * pow(prev_y, 2) + c * prev_u + d * sin(prev_u); // Model 2 update
     }
 
     return { result1, result2 };
 }
 
 int main() {
-    double param_a, param_b, param_c, param_d, temp0;
-    cout << "Please input constant a for the model: ";
-    cin >> param_a;
-    cout << "Please input constant b for the linear model: ";
-    cin >> param_b;
-    cout << "Please input constant c for the nonlinear model: ";
-    cin >> param_c;
-    cout << "Please input constant d for the nonlinear model: ";
-    cin >> param_d;
-    cout << "Please input the initial temperature (y0): ";
-    cin >> temp0;
+    double a, b, c, d, initialTemp;
+    cout << "constant a model: ";
+    cin >> a;
+    cout << "constant b linear model: ";
+    cin >> b;
+    cout << "constant c nonlinear model: ";
+    cin >> c;
+    cout << "constant d nonlinear model: ";
+    cin >> d;
+    cout << "initial temperature (y0): ";
+    cin >> initialTemp;
 
     int numSteps;
-    cout << "Please enter the number of discrete time steps: ";
+    cout << "number of discrete time steps: ";
     cin >> numSteps;
 
-    vector<double> heatValues(numSteps);
-    cout << "Enter the heat values (u) for each time step:" << endl;
+    vector<double> inputs(numSteps);
+    cout << "input values (u) for each time step:" << endl;
     for (int i = 0; i < numSteps; ++i) {
         cout << "u[" << i + 1 << "]: ";
-        cin >> heatValues[i];
+        cin >> inputs[i];
     }
 
-    SimulationData simulationResults = executeSimulation(param_a, param_b, param_c, param_d, temp0, heatValues, numSteps);
+    SimulationData simulationResults = runSimulation(a, b, c, d, initialTemp, inputs, numSteps);
 
-    cout << "\nLinear Model Simulation:" << endl;
+    cout << "\nsimulation results for model 1:" << endl;
     for (int t = 0; t < numSteps; ++t) {
         cout << "Time " << t + 1 << ": Temperature = " << simulationResults.model1[t] << endl;
     }
 
-    cout << "\nNonlinear Model Simulation:" << endl;
+    cout << "\nsimulation results for model 2:" << endl;
     for (int t = 0; t < numSteps; ++t) {
-        cout << "Time " << t + 1 << ": Temperature = " << simulationResults.model2[t] << endl;
+        cout << "time " << t + 1 << ": temperature = " << simulationResults.model2[t] << endl;
     }
 
     return 0;
