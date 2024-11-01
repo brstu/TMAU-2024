3ee1770657547062e74bc9f64b0a9cec52e3d786
Павел <brerikyt@gmail.com>
2024-11-01 14:16:04 +0300

    Update main.cpp

--- trunk/as0006319/task_01/src/main.cpp
+++ trunk/as0006319/task_01/src/main.cpp
@@ -1,52 +1,51 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 using namespace std;
 
-//линейная модель
-void lineynaya(double a, double b, double at, double bt, int t) {
-    cout << "\tЛинейна модель" << endl;
+// Линейная модель
+void lineynaya(double a, double b, double uT, double yT, int t) {
+    cout << "\tЛинейная модель" << endl;
     for (int i = 1; i <= t; i++) {
-        double bt1 = a * bt + b * at;
-        cout << "y" << i << " = " << yT1 << endl;
-        bt = bt1;
+        double yT1 = a * yT + b * uT; // Вычисление yT1
+        cout << "y" << i << " = " << yT1 << endl; // Используем yT1, а не yT
+        yT = yT1; // Обновление yT для следующей итерации
     }
 }
 
-//НЕлинейная модель
-void neLineynaya(double a, double b, double c, double d, double at, double bt, int t) {
-    double at_1 = 0;
-    double bt_1 = 0;
+// Нелинейная модель
+void neLineynaya(double a, double b, double c, double d, double uT, double yT, int t) {
+    double uT_1 = 0;
+    double yT_1 = 0;
 
     cout << "\tНелинейная модель" << endl;
     for (int i = 1; i <= t; i++) {
-        double bt1 = a * bt - b * pow(bt_1, 2) + c * at + d * sin(uT_1);
-        cout << "y" << i << " = " << bt1 << endl;
-        at_1 = at;
-        bt_1 = bt;
-        bt = bt1;
+        double yT1 = a * yT - b * pow(yT_1, 2) + c * uT + d * sin(uT_1);
+        cout << "y" << i << " = " << yT1 << endl;
+        uT_1 = uT;
+        yT_1 = yT;
+        yT = yT1; // Обновление yT для следующей итерации
     }
 }
 
 int main() {
-
     system("chcp 1251");
     system("cls");
 
-    double a, b, c, d; //константы
-    double at; //теплота
-    double bt; //температура
-    int t; // такты
+    double a, b, c, d; // Константы
+    double uT; // Теплота
+    double yT; // Температура
+    int t; // Такты
 
     cout << "Введите константу a: "; cin >> a;
     cout << "Введите константу b: "; cin >> b;
     cout << "Введите константу c: "; cin >> c;
     cout << "Введите константу d: "; cin >> d;
-    cout << "Введите подаваемое тепло: "; cin >> at;
-    cout << "Введите температуру: "; cin >> bt;
+    cout << "Введите подаваемое тепло: "; cin >> uT;
+    cout << "Введите температуру: "; cin >> yT;
     cout << "Введите количество тактов работы модели: "; cin >> t;
 
-    lineynaya(a, b, at, bt, t);
-    neLineynaya(a, b, c, d, at, bt, t);
+    lineynaya(a, b, uT, yT, t);
+    neLineynaya(a, b, c, d, uT, yT, t);
 
     return 0;
 }
