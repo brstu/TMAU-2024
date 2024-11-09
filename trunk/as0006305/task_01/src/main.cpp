95b85c2c0be1a660e7121ed053a8d0278e24d1b8
unknown <pasakasarenko@gmail.com>
2024-11-09 11:13:49 +0300

    lab01

--- trunk/as0006305/task_01/src/main.cpp
+++ trunk/as0006305/task_01/src/main.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 //НЕлинейная модель
-void nonLinear(float a, float b, float c, float d, float uT, float yT, int t) {
+void nonLinearModel(float a, float b, float c, float d, float uT, float yT, int t) {
     float uT_1 = 0;
     float yT_1 = 0;
 
-    cout << "\t---Нелинейная температурная модель---" << endl;
-    for (int i = 1; i <= t; i++) {
+    cout << "Нелинейная температурная модель:" << endl;
+    for (int k = 1; k <= t; k++) {
         double yT1 = a * yT - b * pow(yT_1, 2) + c * uT + d * sin(uT_1);
-        cout << "y" << i << " = " << yT1 << endl;
+        cout << "y" << k << " = " << yT1 << endl;
         uT_1 = uT;
         yT_1 = yT;
         yT = yT1;
@@ -19,11 +19,11 @@ void nonLinear(float a, float b, float c, float d, float uT, float yT, int t) {
 }
 
 //линейная модель
-void linear(float a, float b, float uT, float yT, int t) {
-    cout << "\t---Линейная температурная модель---" << endl;
-    for (int i = 1; i <= t; i++) {
+void linearModel(float a, float b, float uT, float yT, int t) {
+    cout << "Линейная температурная модель:" << endl;
+    for (int k = 1; k <= t; k++) {
         float yT1 = a * yT + b * uT;
-        cout << "y" << i << " = " << yT1 << endl;
+        cout << "y" << k << " = " << yT1 << endl;
         yT = yT1;
     }
 }
@@ -47,17 +47,20 @@ int main()
     int t;
 
     cout << "Введите константы" << endl;
+    
     cout << "a: "; cin >> a;
+    
     cout << "b: "; cin >> b;
+    
     cout << "c: "; cin >> c;
+    
     cout << "d: "; cin >> d;
     
-    cout << "Введите подаваемое тепло: "; cin >> uT;
-    cout << "Введите температуру: "; cin >> yT;
-    cout << "Введите к-во тактов работы модели: "; cin >> t;
+    cout << "Подаваемое тепло: "; cin >> uT;
+    cout << "Температура: "; cin >> yT;
+    cout << "К-во тактов работы модели: "; cin >> t;
     
-    nonLinear(a, b, c, d, uT, yT, t);
-    linear(a, b, uT, yT, t);
-    
-    return 0;
+    //функции
+    nonLinearModel(a, b, c, d, uT, yT, t);
+    linearModel(a, b, uT, yT, t);
 }
