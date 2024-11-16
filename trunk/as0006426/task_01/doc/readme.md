aa685d1af8b0d8b83923c9050853c6b00b854b6a
artby04 <a.s.yarmoluk@gmail.com>
2024-11-16 09:12:57 +0300

    [task_01] Fix task01

--- trunk/as0006426/task_01/doc/readme.md
+++ trunk/as0006426/task_01/doc/readme.md
@@ -1,121 +1,48 @@
-# Лабораторная работа №1
+<p align="center"> Министерство образования Республики Беларусь</p>
+<p align="center">Учреждение образования</p>
+<p align="center">“Брестский Государственный технический университет”</p>
+<p align="center">Кафедра ИИТ</p>
+<br><br><br><br><br><br><br>
+<p align="center">Лабораторная работа №1</p>
+<p align="center">По дисциплине “Теория и методы автоматического управления”</p>
+<p align="center">Тема: “Моделирования температуры объекта”</p>
+<br><br><br><br><br>
+<p align="right">Выполнил:</p>
+<p align="right">Студент 3 курса</p>
+<p align="right">Группы АС-61</p>
+<p align="right">Ярмолюк А.С.</p>
+<p align="right">Проверил:</p>
+<p align="right">Ситковец Я. С.</p>
+<br><br><br><br><br>
+<p align="center">Брест 2023</p>
 
-## Task:
+---
+
+**Задание**:
 
 Let's get some object to be controlled. We want to control its temperature, which can be described by this differential equation:
 
-$$
-\Large\frac{dy(\tau)}{d\tau}=\frac{u(\tau)}{C}+\frac{Y_0-y(\tau)}{RC}(1)
-$$
+$$\Large\frac{dy(\tau)}{d\tau}=\frac{u(\tau)}{C}+\frac{Y_0-y(\tau)}{RC} $$ (1)
 
-where τ – time; y ( τ ) – input temperature; u ( τ ) – input warm; Y 0 – room temperature; C , R C – some constants.
+where $\tau$ – time; $y(\tau)$ – input temperature; $u(\tau)$ – input warm; $Y_0$ – room temperature; $C,RC$ – some constants.
 
-After transformation we get these linear (2) and nonlinear (3) models:
+Task is to write program (**С++**), which simulates this object temperature.
 
-$$
-\Large y_{\tau+1}=ay_{\tau}+bu_{\tau}(2)
-$$
+OUT Progam:
 
-$$
-\Large y_{\tau+1}=ay_{\tau}-by_{\tau-1}^2+cu_{\tau}+d\sin(u_{\tau-1})(3)
-$$
-
-where τ – time discrete moments ( 1 , 2 , 3 … n ); a , b , c , d – some constants.
-
-Task is to write program (С++), which simulates this object temperature.
-Example of program output:
-
+```bash
+Input time param: 7
+Input initial temper: 2
+Linear model data
+{ y(t): 4.00; t: 1 }
+{ y(t): 3.00; t: 2 }
+{ y(t): 2.50; t: 3 }
+{ y(t): 2.25; t: 4 }
+{ y(t): 2.13; t: 5 }
+Non linear model data
+{ y(t): 4.20; t: 1 }
+{ y(t): 51.00; t: 2 }
+{ y(t): 1634561.46; t: 3 }
+{ y(t): -13685.33; t: 4 }
+{ y(t): -113716418.34; t: 5 }
 ```
-Choose an option (0 - exit, 1 - linear model, 2 - nonlinear model): Input choice: 1
-Input A: 10
-Input B: 3
-Input current temperature: 35
-Input heat input: 5
-Input simulation time: 10
-TIME      Linear Model
-=========================
-1         438
-2         5238
-3         62838
-4         754038
-5         9.04844e+06
-6         1.08581e+08
-7         1.30297e+09
-8         1.56357e+10
-9         1.87628e+11
-10        2.25154e+12
-
-Choose an option (0 - exit, 1 - linear model, 2 - nonlinear model): Input choice: 2
-Input A: 4
-Input B: 10
-Input C: 25
-Input D: 30
-Input current temperature: 28
-Input heat input: 40
-Input simulation time: 20
-TIME      Nonlinear Model
-=========================
-1         812.912
-2         -7896.35
-3         -9.7409e+06
-4         -9.40969e+08
-5         -1.42328e+15
-6         -1.32856e+19
-7         -3.03859e+31
-8         -2.64761e+39
-
-artyomyarmoluk@Artyoms-MacBook-Air uni/TMAU-2024/as006426/task_01/src> ./lab_01.exe
-
-Choose an option (0 - exit, 1 - linear model, 2 - nonlinear model): Input choice: 1
-Input A: 1.0
-Input B: 0.6
-Input current temperature: 26
-Input heat input: 12
-Input simulation time: 5
-TIME      Linear Model
-=========================
-1         32.8
-2         41.68
-3         51.344
-4         61.1175
-5         71.8385
-
-Choose an option (0 - exit, 1 - linear model, 2 - nonlinear model): Input choice: 2
-Input A: 1.3
-Input B: 0.2
-Input C: 0.4
-Input D: 0.1
-Input current temperature: 32
-Input heat input: 18
-Input simulation time: 5
-TIME      Nonlinear Model
-=========================
-1         42.5325
-2         -37.9285
-3         -210.95
-4         -380.841
-```
-
-## Input erroe handling
-
-```
-Choose an option (0 - exit, 1 - linear model, 2 - nonlinear model): Input choice: p
-
-*** Incorrect input ***
-Input choice: 5
-*** Invalid input ***
-```
-
-## Exit the program
-
-```
-Choose an option (0 - exit, 1 - linear model, 2 - nonlinear model): Input choice: 0
-artyomyarmoluk@Artyoms-MacBook-Air uni/TMAU-2024trunk/as006426/task_01/src> ^C
-```
-
-The source code
-
-The source code is located in the src folder.
-
-    CMakeLists.txt - this is a file that contains CMake commands to control the project build process.
-    main.cpp - source file, contains the main function that controls the program execution.
--- trunk/as0006426/task_01/src/main.cpp
+++ trunk/as0006426/task_01/src/main.cpp
@@ -1,92 +1,132 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
-#include <map>
-#include <limits>
+#include <string>
 
 using namespace std;
 
-void execute_linear_simulation();
-void execute_nonlinear_simulation();
-bool retrieve_choice(int &selection);
 
-void obtain_input(double &value, const string &message) {
-    cout << message << ": ";
-    while (!(cin >> value)) {
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid input! Please try again.\n" << message << ": ";
+
+
+class temMod {
+public:
+
+    double temper;
+
+
+    double war;
+
+
+    int t;
+
+    temMod(double temper, double war, int t) : temper(temper), war(war), t(t)  {
+    }
+
+     string toString() const {
+
+            return "{ y(t): " + to_string(this->temper) + "; t: " + to_string(this->t) + " }";
+    }
+
+
+};
+
+
+ const double A = 0.5;
+
+
+
+ const double B = 1;
+
+
+ const double C = 1.2;
+
+
+ const double D = 0.4;
+
+
+ const double WARM = 1;
+
+
+
+void showData(const vector<temMod>& temperMods) {
+    for (const auto& model : temperMods) {
+        cout << model.toString() << endl;
     }
 }
 
-void obtain_input(int &value, const string &message) {
-    cout << message << ": ";
-    while (!(cin >> value)) {
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid input! Please try again.\n" << message << ": ";
+
+
+
+
+
+
+vector<temMod> simulateLinearMod(int time, double temper) {
+    vector<temMod> lineartempers;
+
+    for (int t = 1; t <= time; t++) {
+    double curwar = WARM;
+    temper = round((A * temper + B * curwar) * 100) / 100;
+    lineartempers.emplace_back(temper, curwar, t);
     }
+
+    
+
+    return lineartempers;
 }
 
-void show_results(const map<int, double> &temperatures, const string &model_name) {
-    cout << "\n--- Results for " << model_name << " ---\n";
-    for (const auto &entry : temperatures) {
-        cout << "Step " << entry.first << ": Temperature = " << entry.second << endl;
+
+
+vector<temMod> simulateNonLinearMod(int time, double temper) {
+        vector<temMod> nonLineartempers;
+        double prtemper = 0;
+        double prwar = 0;
+
+
+
+    for (int t = 1; t <= time; t++) {
+
+
+    double curwar = WARM;
+
+
+    temper = A * temper - B * pow(prtemper, 2) + C * curwar + D * sin(prwar);
+
+
+    temper = round(temper * 100) / 100;
+
+
+    nonLineartempers.emplace_back(temper, curwar, t);
+    prtemper = temper;
+
+
+    prwar = curwar;
     }
+
+
+
+    return nonLineartempers;
 }
 
-void execute_linear_simulation() {
-    double coefA, coefB, initial_temp, heat_input;
-    int total_steps;
-    obtain_input(coefA, "Enter coefficient A");
-    obtain_input(coefB, "Enter coefficient B");
-    obtain_input(initial_temp, "Enter initial temperature");
-    obtain_input(heat_input, "Enter heat input");
-    obtain_input(total_steps, "Enter number of steps");
 
-    map<int, double> results;
-    for (int step = 1; step <= total_steps; ++step) {
-        initial_temp = coefA * initial_temp + coefB * heat_input;
-        results[step] = initial_temp;
-    }
-    show_results(results, "Linear Model");
-}
 
-void execute_nonlinear_simulation() {
-    double coefA, coefB, coefC, coefD, initial_temp, heat_input;
-    int total_steps;
-    obtain_input(coefA, "Enter coefficient A");
-    obtain_input(coefB, "Enter coefficient B");
-    obtain_input(coefC, "Enter coefficient C");
-    obtain_input(coefD, "Enter coefficient D");
-    obtain_input(initial_temp, "Enter initial temperature");
-    obtain_input(heat_input, "Enter heat input");
-    obtain_input(total_steps, "Enter number of steps");
 
-    map<int, double> results;
-    double previous_temp = 0;
-    for (int step = 1; step <= total_steps; ++step) {
-        double new_temp = coefA * initial_temp - coefB * pow(previous_temp, 2) + coefC * heat_input + coefD * sin(heat_input);
-        previous_temp = initial_temp;
-        initial_temp = new_temp;
-        results[step] = initial_temp;
-    }
-    show_results(results, "Nonlinear Model");
-}
+int main()
+{
+    int time;
+    cout << "Input time parameter: ";
+    cin >> time;
 
-bool retrieve_choice(int &selection) {
-    obtain_input(selection, "Select an option (0 - Exit, 1 - Linear Model, 2 - Nonlinear Model)");
-    return selection >= 0 && selection <= 2;
-}
+    int temper;
+    cout << "Input initial temperature: ";
+    cin >> temper;
 
-int main() {
-    void (*simulation_methods[])() = { []() { exit(0); }, execute_linear_simulation, execute_nonlinear_simulation };
-    int selection;
+    cout << "Linear model data" << endl;
+    auto linear = simulateLinearMod(time, temper);
+    showData(linear);
 
-    while (true) {
-        if (!retrieve_choice(selection)) {
-            cout << "Invalid selection! Please try again.\n";
-        } else {
-            simulation_methods[selection]();
-        }
-    }
+    cout << "Non linear model data" << endl;
+    auto nonLinear = simulateNonLinearMod(time, temper);
+    showData(nonLinear);
+
+    return 0;
 }
