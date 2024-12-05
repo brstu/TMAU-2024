5111429c423cd98e38e30ad3e64a303135c4c098
Kirill Ignatkevich <maratandkirill24@gmail.com>
2024-12-05 19:39:22 +0300

    [task_02] Add solution

--- trunk/as0006442/task_02/src/lab2.cpp
+++ trunk/as0006442/task_02/src/lab2.cpp
@@ -6,22 +6,21 @@ using namespace std;
 class Model {
 private:
     double a, b, c, d;
-    double Prew_Y = 0;
-    double Prew_U = 0;
-    bool is_linear;
+    double prevY = 0;
+    double prevU = 0;
+    bool isLinear;
 
 public:
-    Model(double A, double B, double c = 0, double d = 0, bool is_linear = true)
-        : a(A), b(B), c(c), d(d), is_linear(is_linear) {}
+    Model(double a, double b, double c = 0, double d = 0, bool isLinear = true)
+        : a(a), b(b), c(c), d(d), isLinear(isLinear) {}
 
-    double get_temp(double Y, double U) {
-        if (is_linear) {
+    double getTemp(double Y, double U) {
+        if (isLinear) {
             return a * Y + b * U;
-        }
-        else {
-            double res = a * Y - b * pow(Prew_Y, 2) + c * U + d * sin(Prew_U);
-            Prew_Y = Y;
-            Prew_U = U;
+        } else {
+            double res = a * Y - b * pow(prevY, 2) + c * U + d * sin(prevU);
+            prevY = Y;
+            prevU = U;
             return res;
         }
     }
@@ -29,29 +28,29 @@ public:
 
 class PID {
 private:
-    const double T_0 = 30;
+    const double T0 = 30;
     const double Time = 10;
     double Uk = 0;
     const double k = 0.5;
     const double T = 25;
-    const double T_d = 15;
+    const double Td = 15;
 
-    double get_Uk(double e, double e1, double e2) {
-        double q0 = k * (1 + (T_d / T_0));
-        double q1 = -k * (1 + 2 * (T_d / T_0) - (T_0 / T));
-        double q2 = k * (T_d / T_0);
+    double getUk(double e, double e1, double e2) {
+        double q0 = k * (1 + (Td / T0));
+        double q1 = -k * (1 + 2 * (Td / T0) - (T0 / T));
+        double q2 = k * (Td / T0);
         Uk += q0 * e + q1 * e1 + q2 * e2;
         return Uk;
     }
 
 public:
 
-    void regulAtor(double W, double Y0, Model& model) {
+    void regulator(double W, double Y0, Model& model) {
         double e2 = 0, e1 = 0, y = Y0;
         for (int i = 1; i <= Time; i++) {
             double e = W - y;
-            Uk = get_Uk(e, e1, e2);
-            y = model.get_temp(Y0, Uk);
+            Uk = getUk(e, e1, e2);
+            y = model.getTemp(Y0, Uk);
             cout << "E = " << e << ", Yt = " << y << ", Uk = " << Uk << endl;
             e2 = e1;
             e1 = e;
@@ -60,10 +59,10 @@ public:
     }
 };
 
-void input_par(double& A, double& B, double& c, double& d, bool is_nonlinearModel) {
+void inputParser(double& A, double& B, double& c, double& d, bool isNonlinearModel) {
     cout << "Enter A: "; cin >> A;
     cout << "Enter B: "; cin >> B;
-    if (is_nonlinearModel) {
+    if (isNonlinearModel) {
         cout << "Enter c: "; cin >> c;
         cout << "Enter d: "; cin >> d;
     }
@@ -76,12 +75,12 @@ int main() {
     const double B = 0.64;
     const double C = 0.80;
     const double D = 0.07;
-    Model linear_model{ A, B, true };
-    Model nonlinear_model{ A, B, C, D, false };
-    PID pid_regulAtor;
-    cout << "Not Linear mode" << endl;
-    pid_regulAtor.regulAtor(W, Y0, nonlinear_model);
+    Model linearModel{ A, B, true };
+    Model nonlinearModel{ A, B, C, D, false };
+    PID pidRegulator;
+    cout << "Nonlinear mode" << endl;
+    pidRegulator.regulator(W, Y0, nonlinearModel);
     cout << "Linear mode" << endl;
-    pid_regulAtor.regulAtor(W, Y0, linear_model);
+    pidRegulator.regulator(W, Y0, linearModel);
     return 0;
 }
