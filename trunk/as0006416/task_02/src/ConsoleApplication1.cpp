da7309ae16d0c48f20e876537b4ab49999472fa1
Maxim Rabchenia <maxbenwer@gmail.com>
2024-12-06 20:06:21 +0300

    [task_02] Add solution

--- trunk/as0006416/task_02/src/ConsoleApplication1.cpp
+++ trunk/as0006416/task_02/src/ConsoleApplication1.cpp
@@ -1,41 +1,40 @@
 ﻿#include <iostream>
-#include<cmath>
-// Линейная модель
-double linearModel(double Yt, double U, double a, double b) {
-    return a * Yt + b * U;
-}
+#include <cmath>
 
-// Нелинейная модель
-double nonlinearModel(double Yt, double Yt_1, double U, double a, double b, double c, double d) {
-    return a * Yt - b * pow(Yt_1, 2) + c * U + d * sin(U);
+// Общая функция модели
+double model(bool isLinear, double Yt, double Yt_1, double U, double a, double b, double c = 0, double d = 0) {
+    if (isLinear) {
+        return a * Yt + b * U; // Линейная модель
+    } else {
+        return a * Yt - b * pow(Yt_1, 2) + c * U + d * sin(U); // Нелинейная модель
+    }
 }
 
 int main() {
+    // Параметры моделей
+    double a = 0.8;
+    double b = 0.5;
+    double c = 0.2;
+    double d = 0.1;
 
-    double a_linear = 0.8;
-    double b_linear = 0.5;
-    double a_nonlinear = 0.8;
-    double b_nonlinear = 0.5;
-    double c_nonlinear = 0.2;
-    double d_nonlinear = 0.1;
-
-
+    // Начальные значения
     double Y_linear = 0.0;
     double Y_nonlinear = 0.0;
     double Y_prev_nonlinear = 0.0;
 
-    // Входные значения
+    // Входное значение
     double U = 1.0;
 
     // Моделирование для 10 временных шагов
     for (int t = 1; t <= 10; t++) {
         // Линейная модель
-        Y_linear = linearModel(Y_linear, U, a_linear, b_linear);
+        Y_linear = model(true, Y_linear, 0, U, a, b);
 
         // Нелинейная модель
-        Y_nonlinear = nonlinearModel(Y_nonlinear, Y_prev_nonlinear, U, a_nonlinear, b_nonlinear, c_nonlinear, d_nonlinear);
+        Y_nonlinear = model(false, Y_nonlinear, Y_prev_nonlinear, U, a, b, c, d);
         Y_prev_nonlinear = Y_nonlinear;
 
+        // Вывод результатов
         std::cout << "time step: " << t << std::endl;
         std::cout << "Linear model: " << Y_linear << std::endl;
         std::cout << "Nonlinear model: " << Y_nonlinear << std::endl;
